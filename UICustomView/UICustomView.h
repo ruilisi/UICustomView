04335fff44d232233d0df0d6fc592cd9a566eab7
徐恒一 <920301419@qq.com>
2019-03-12 17:42:38 +0800

    init UICustomView cocoaPods

---
+++ UICustomView/UICustomView.h
@@ -0,0 +1,19 @@
+//
+//  UICustomView.h
+//  UICustomView
+//
+//  Created by 徐恒一 on 2019/3/12.
+//  Copyright © 2019 徐恒一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICustomView.
+FOUNDATION_EXPORT double UICustomViewVersionNumber;
+
+//! Project version string for UICustomView.
+FOUNDATION_EXPORT const unsigned char UICustomViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICustomView/PublicHeader.h>
+
+
